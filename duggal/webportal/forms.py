05021e1730280966129a88301b06fa6b696f86b6
singh1114 <ranvir.singh1114@gmail.com>
2016-07-31 19:54:56 +0530

    Code cleaned

--- duggal/webportal/forms.py
+++ duggal/webportal/forms.py
@@ -5,31 +5,20 @@ from .models import *
 class cementform(forms.Form):
 	grade = Cement.grade
 	#print(grade)
-	temp = Cement.objects.order_by().values('Company_Name').distinct()
-	temp5 = Cement.objects.order_by().values('id').distinct()
+	distcompanyname = Cement.objects.order_by().values('Company_Name').distinct()
+	ids = Cement.objects.order_by().values('id').distinct()
 	#print(temp)
-	temp2 =()
-	temp3 = 0
-	for temp1 in temp:
-		temp2 = ((temp5[temp3]['id'], temp1['Company_Name']),) + temp2
-		temp3 = temp3+1
+	companynametuple =()
+	tempvar1 = 0
+	for tempvar2 in distcompanyname:
+		companynametuple = ((ids[tempvar1]['id'], tempvar2['Company_Name']),) + companynametuple
+		tempvar1 = tempvar1 + 1
 
-	print(temp2)
-	companies_name = Cement.objects.all()
-	# loop to create a variable with different choices at list key
-	companies_name = tuple(companies_name)
-	print(companies_name)
-	x = 0
-	z = ()
-	for q in companies_name:
-		z=((q.id, q.Company_Name),)+z
-		x = x+1
-
-	print(z)
-	z = tuple(z)
+	print(companynametuple)
+	
 
 	#companies_name = companies_name.Company_Name
-	company_name = forms.ChoiceField(choices=temp2)
+	company_name = forms.ChoiceField(choices=companynametuple)
 	Type = forms.CharField()
 	Grade = forms.CharField()
 	bag_numbers = forms.IntegerField()
